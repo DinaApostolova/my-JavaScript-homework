9ab465496a50e7805396fdd3a173e8c55987a4f3
DinaApostolova <119695079+DinaApostolova@users.noreply.github.com>
2022-12-02 18:43:34 +0100

    Add files via upload

---
+++ JavaScript Homework 1/CODE/app.js
@@ -0,0 +1,8 @@
+let priceOnePhone=119.95;
+let numberTwo=30;
+let priceThirtyPhones= priceOnePhone * numberTwo;
+let taxRate=5;
+let modulusResult=taxRate % priceThirtyPhones;
+let finalResult=modulusResult + priceThirtyPhones;
+console.log=("The price is:", finalResult);
+
---
+++ JavaScript exercise 3/CODE/app.js
@@ -0,0 +1,5 @@
+let numberPi=3.14;
+let radiusCircle=12;
+let radiusCircleTwo=radiusCircle * radiusCircle;
+let circleArea=numberPi * radiusCircleTwo;
+console.log=("Circle Area is:", circleArea);

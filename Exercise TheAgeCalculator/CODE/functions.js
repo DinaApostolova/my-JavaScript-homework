5e456e554ae8f76327b2a430811ba5e768e883d1
DinaApostolova <119695079+DinaApostolova@users.noreply.github.com>
2022-12-08 21:45:16 +0100

    Add files via upload

---
+++ Exercise TheAgeCalculator/CODE/functions.js
@@ -0,0 +1,9 @@
+console.log("THE AGE CALCULATOR")
+console.log("Forgot how old you are? Calculate it!")
+function calculateAge(currentYear, birthYear){
+    let finalResult = currentYear - birthYear
+    console.log("You are ", finalResult, "years old.")
+}
+calculateAge(2022, 1992)
+calculateAge(2022, 1997)
+calculateAge(2022, 2001)

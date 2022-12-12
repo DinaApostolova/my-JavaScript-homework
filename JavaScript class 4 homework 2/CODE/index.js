63bc5e04f8fb0762eefa1bd227a1e2156b609393
DinaApostolova <119695079+DinaApostolova@users.noreply.github.com>
2022-12-12 16:55:10 +0100

    Add files via upload

---
+++ JavaScript class 4 homework 2/CODE/index.js
@@ -0,0 +1,10 @@
+console.log("CONNECTED")
+function sumOfFiveNumbers(numberOne, numberTwo, numberThree, numberFour, numberFive){
+    let result = numberOne + numberTwo + numberThree + numberFour + numberFive
+    return result
+}
+let finalSum = sumOfFiveNumbers(10, 20, 30, 40, 50)
+console.log (`Sum of five number is:`, finalSum)
+console.log (`Sum of five number is:`, (15, 25, 32, 100, 250))
+console.log (`Sum of five number is:`, (0.2, 3.7, 4.2, 1, 2.5))
+

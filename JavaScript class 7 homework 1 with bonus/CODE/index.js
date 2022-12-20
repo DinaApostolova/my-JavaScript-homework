b35de01a51ef5f044bd9b865543f1fc81dc95819
DinaApostolova <119695079+DinaApostolova@users.noreply.github.com>
2022-12-20 15:11:21 +0100

    Add files via upload

---
+++ JavaScript class 7 homework 1 with bonus/CODE/index.js
@@ -0,0 +1,6 @@
+console.log("CONNECTED")
+let animalNameInput = document.getElementById("animalNameInput")
+let animalSpeakBtn = document.getElementById("animalSpeakBtn")
+
+console.log(animalNameInput, "says:`Hey bro!!!`")
+console.log(animalSpeakBtn, "says:`Hey bro!!!`")
